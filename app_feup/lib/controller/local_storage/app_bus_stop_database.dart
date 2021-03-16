d4c43ed9bc7f52441d097324f6274db5ddc87b2c
André Lima <me@limwa.pt>
2021-03-16 20:06:09 +0000

    docs: :memo: document a few more files

--- app_feup/lib/controller/local_storage/app_bus_stop_database.dart
+++ app_feup/lib/controller/local_storage/app_bus_stop_database.dart
@@ -4,12 +4,23 @@ import 'package:sqflite/sqflite.dart';
 import 'package:collection/collection.dart';
 import 'package:uni/model/entities/bus_stop.dart';
 
+/// Manages the app's Bus Stops database.
+/// 
+/// This database stores information about the bus stops that the user
+/// wants to keep track of. It also stores information about
+/// which ones are the user's favorite stops.
 class AppBusStopDatabase extends AppDatabase {
   AppBusStopDatabase()
       : super('busstops.db', [
           'CREATE TABLE busstops(stopCode TEXT, busCode TEXT)',
           'CREATE TABLE favoritestops(stopCode TEXT, favorited TEXT)'
         ]);
+
+  /// Returns a map containing all the data available in this database.
+  /// 
+  /// *Note:*
+  /// * a key in this map is a bus stop's `stopCode`.
+  /// * a value in this map is the corresponding [BusStopData] instance.
   Future<Map<String, BusStopData>> busStops() async {
     // Get a reference to the database
     final Database db = await this.getDatabase();
@@ -33,6 +44,7 @@ class AppBusStopDatabase extends AppDatabase {
     return stops;
   }
 
+  /// Toggles whether or not a bus stop is considered a user's favorite.
   Future<void> updateFavoriteBusStop(String stopCode) async {
     final Map<String, BusStopData> stops = await busStops();
     stops[stopCode].favorited = !stops[stopCode].favorited;
@@ -40,6 +52,7 @@ class AppBusStopDatabase extends AppDatabase {
     await _insertBusStops(stops);
   }
 
+  /// Adds a bus stop to this database.
   Future<void> addBusStop(String stopCode, BusStopData stopData) async {
     final Map<String, BusStopData> stops = await busStops();
     stops[stopCode] = stopData;
@@ -47,6 +60,7 @@ class AppBusStopDatabase extends AppDatabase {
     await _insertBusStops(stops);
   }
 
+  /// Removes a bus stop from this database.
   Future<void> removeBusStop(String stopCode) async {
     final Map<String, BusStopData> stops = await busStops();
     stops.remove(stopCode);
@@ -54,6 +68,9 @@ class AppBusStopDatabase extends AppDatabase {
     await _insertBusStops(stops);
   }
 
+  /// Adds all entries from [stops] to this database.
+  /// 
+  /// If a row with the same data is present, it will be replaced.
   Future<void> _insertBusStops(Map<String, BusStopData> stops) async {
     stops.forEach((stopCode, stopData) async {
       await insertInDatabase('favoritestops',
@@ -71,12 +88,16 @@ class AppBusStopDatabase extends AppDatabase {
     });
   }
 
+  // TODO Should this doc be more specific or is this fine?
+  /// Deletes all data from this database.
   Future<void> deleteBusStops() async {
     // Get a reference to the database
     final Database db = await this.getDatabase();
     await db.delete('busstops');
   }
 
+  /// Replaces all of the data in this database with entries
+  /// from [stops].
   Future<void> setBusStops(Map<String, BusStopData> stops) async {
     await deleteBusStops();
     await _insertBusStops(stops);
