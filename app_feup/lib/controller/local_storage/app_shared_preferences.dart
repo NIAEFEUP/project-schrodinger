2cf0d4eafba7edadb7544a891e333f4e1dd3538d
André Lima <me@limwa.pt>
2021-04-07 19:32:43 +0100

    docs: :memo: document app_shared_preferences.dart

--- app_feup/lib/controller/local_storage/app_shared_preferences.dart
+++ app_feup/lib/controller/local_storage/app_shared_preferences.dart
@@ -6,6 +6,10 @@ import 'package:encrypt/encrypt.dart';
 import 'package:tuple/tuple.dart';
 import 'package:uni/model/home_page_model.dart';
 
+/// Manages the app's Shared Preferences.
+/// 
+/// This database stores the user's student number, password and favorite
+/// widgets.
 class AppSharedPreferences {
   static final String userNumber = 'user_number';
   static final String userPw = 'user_password';
@@ -18,30 +22,40 @@ class AppSharedPreferences {
     FAVORITE_WIDGET_TYPE.busStops
   ];
 
+  /// Saves the user's student number and password.
   static Future savePersistentUserInfo(user, pass) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString(userNumber, user);
     prefs.setString(userPw, encode(pass));
   }
 
+  /// Deletes the user's student number and passoword.
   static Future removePersistentUserInfo() async {
     final prefs = await SharedPreferences.getInstance();
     prefs.remove(userNumber);
     prefs.remove(userPw);
   }
 
+  /// Returns a tuple containing the user's student number and password.
+  /// 
+  /// *Note:*
+  /// * the first element in the tuple is the user's student number.
+  /// * the second element in the tuple is the user's password, in plain text
+  /// format.
   static Future<Tuple2<String, String>> getPersistentUserInfo() async {
     final String userNum = await getUserNumber();
     final String userPass = await getUserPassword();
     return  Tuple2(userNum, userPass);
   }
 
+  /// Returns the user's student number.
   static Future<String> getUserNumber() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString(userNumber) ??
         ''; // empty string for the case it does not exist
   }
 
+  /// Returns the user's password, in plain text format.
   static Future<String> getUserPassword() async {
     final prefs = await SharedPreferences.getInstance();
     String pass = prefs.getString(userPw) ?? '';
@@ -55,12 +69,14 @@ class AppSharedPreferences {
     return pass;
   }
 
+  /// Replaces the user's favorite widgets with [newFavorites].
   static saveFavoriteCards(List<FAVORITE_WIDGET_TYPE> newFavorites) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setStringList(
         favoriteCards, newFavorites.map((a) => a.index.toString()).toList());
   }
 
+  /// Returns a list containing the user's favorite widgets.
   static Future<List<FAVORITE_WIDGET_TYPE>> getFavoriteCards() async {
     final prefs = await SharedPreferences.getInstance();
     final List<String> storedFavorites = prefs.getStringList(favoriteCards);
@@ -71,16 +87,19 @@ class AppSharedPreferences {
         defaultFavoriteCards;
   }
 
+  /// Encrypts [plainText] and returns its Base64 representation.
   static String encode(String plainText) {
     final encrypter = _createEncrypter();
     return encrypter.encrypt(plainText).base64;
   }
 
+  /// Decrypts [base64Text].
   static String decode(String base64Text) {
     final encrypter = _createEncrypter();
     return encrypter.decrypt64(base64Text);
   }
 
+  /// Creates an [Encrypter] for encrypting and decrypting the user's password.
   static Encrypter _createEncrypter() {
     final key = Key.fromLength(keyLength);
     final iv = IV.fromLength(ivLength);
